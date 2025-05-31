cc277d98bf7af4b729dd6065b30906cb0039567c
Lamrikhi Abdessamad <lamrikhiabdessamad@gmail.com>
2025-05-31 09:40:51 +0100

    ajout de couleurs

--- log-analyzer.py
+++ log-analyzer.py
@@ -1,4 +1,7 @@
-# log-analyzer.py
+# version améliorée log-analyzer.py
+from colorama import Fore, Style, init
+
+init()
 
 def analyser_log(fichier_log):
     stats = {"INFO": 0, "WARNING": 0, "ERROR": 0}
@@ -8,13 +11,19 @@ def analyser_log(fichier_log):
             for niveau in stats:
                 if ligne.startswith(niveau):
                     stats[niveau] += 1
+                    if niveau == "INFO":
+                        print(Fore.BLUE + ligne.strip())
+                    elif niveau == "WARNING":
+                        print(Fore.YELLOW + ligne.strip())
+                    elif niveau == "ERROR":
+                        print(Fore.RED + ligne.strip())
 
     with open("rapport.txt", "w") as rapport:
         rapport.write("=== Rapport de Log ===\n")
         for niveau, count in stats.items():
             rapport.write(f"{niveau}: {count}\n")
 
-    print("Rapport généré dans 'rapport.txt'.")
+    print(Style.RESET_ALL + "Rapport généré dans 'rapport.txt'.")
 
 if __name__ == "__main__":
     analyser_log("log.txt")
